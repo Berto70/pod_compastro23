127a309c2e9d3a7a84b7a0a437271691ef3ed33e
giulianoiorio <giuliano.iorio89@gmail.com>
2023-10-14 23:34:30 +0200

    Added the ic generator function ic_two_body and the related tests

--- Fireworks/tests/test_ic.py
+++ Fireworks/tests/test_ic.py
@@ -15,3 +15,41 @@ def test_ic_random_normal():
     assert len(particles)==N #Test if we create the right amount of particles
     assert np.all(particles.mass==10.) #Test if the mass of the particles is set correctly
 
+def test_ic_two_body_circular():
+    """
+    Simple test for equal mass stars in a circular orbit
+    """
+    mass1=1.
+    mass2=1.
+    Mtot=mass1+mass2
+    rp=2.
+    particles = fic.ic_two_body(mass1,mass2,rp=rp,e=0.)
+
+    assert pytest.approx(particles.vel[0,1],1e-10) == -1./Mtot
+    assert pytest.approx(particles.vel[1,1],1e-10) == 1./Mtot
+
+def test_ic_two_body_parabolic():
+    """
+    Simple test for equal mass stars in a parabolic orbit
+    """
+    mass1=1.
+    mass2=1.
+    Mtot=mass1+mass2
+    rp=2.
+    particles = fic.ic_two_body(mass1,mass2,rp=rp,e=1.)
+
+    assert pytest.approx(particles.vel[0,1],1e-10) == -np.sqrt(2)/Mtot
+    assert pytest.approx(particles.vel[1,1],1e-10) == np.sqrt(2)/Mtot
+
+def test_ic_two_body_hyperbolic():
+    """
+    Simple test for equal mass stars in a hyperbolic orbit
+    """
+    mass1=1.
+    mass2=1.
+    Mtot=mass1+mass2
+    rp=2.
+    particles = fic.ic_two_body(mass1,mass2,rp=rp,e=3.)
+
+    assert pytest.approx(particles.vel[0,1],1e-10) == -2./Mtot
+    assert pytest.approx(particles.vel[1,1],1e-10) == 2./Mtot
