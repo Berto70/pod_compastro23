b5902e4ef39488142a0ab258a5e59c1f096a1369
diegobonato <diegobonato99@gmail.com>
2023-11-05 11:38:07 +0000

    trying Testing

--- Fireworks/fireworks/nbodylib/dynamics.py
+++ Fireworks/fireworks/nbodylib/dynamics.py
@@ -42,7 +42,7 @@ except:
 def acceleration_estimate_template(particles: Particles, softening: float =0.) \
         -> Tuple[npt.NDArray[np.float64],Optional[npt.NDArray[np.float64]],Optional[npt.NDArray[np.float64]]]:
     """
-    This an empty functions that can be used as a basic template for
+    This an empty function that can be used as a basic template for
     implementing the other functions to estimate the gravitational acceleration.
     Every function of this kind needs to have two input parameters:
 
@@ -98,3 +98,120 @@ def acceleration_pyfalcon(particles: Particles, softening: float =0.) \
     jerk = None
 
     return acc, jerk, pot
+
+
+def acceleration_direct(particles: Particles, softening: float =0.) \
+        -> Tuple[npt.NDArray[np.float64],Optional[npt.NDArray[np.float64]],Optional[npt.NDArray[np.float64]]]:
+  
+  
+  #acc  = np.zeros(len(particles))
+  jerk = None
+  pot = None
+
+  pos  = particles.pos
+  #vel  = particles.vel
+  mass = particles.mass
+  N    = len(particles) # ? could be 
+
+  # Diego: sposto qui acc e lo rendo una matrice, cosi acc[0,:] ax,ay,az della particella 0 
+  acc  = np.zeros([N,3])
+
+  # Idea: put condition : if softening!= 0 direct brute estimate, otherwise use softening
+
+  # Using direct force estimate applcation2 - see slides Lecture 3 p.16
+
+
+  def acc_2body(position_1,position_2,mass_2):
+      
+      """
+      Implements definition of acceleration for two bodies i,j
+    
+      This is used in the following for loop
+      """
+      # Cartesian component of the i,j particles distance
+      dx = position_1[0] - position_2[0]
+      dy = position_1[1] - position_2[1]
+      dz = position_1[2] - position_2[2]
+      
+
+      # Distance module
+      r = np.sqrt(dx**2 + dy**2 + dz**2)
+
+      # Cartesian component of the i,j force
+
+      ## Diego: question -> shouldn't acceleration be divided my mass1?
+      acceleration = np.zeros(3)
+      acceleration[0] = mass_2 * dx / r**3
+      acceleration[1] = mass_2 * dy / r**3
+      acceleration[2] = mass_2 * dz / r**3
+
+      return acceleration
+  
+  def direct_acc_no_softening(mass=mass): 
+        
+        for i in range(N-1):
+            for j in range(i+1,N):
+                # Compute relative acceleration given
+                # position of particle i and j
+                mass_1 = mass[i]
+                mass_2 = mass[j]
+                acc_ij = acc_2body(position_1=pos[i,:],position_2=pos[j,:],mass_2=mass_2)
+
+                # Update array with accelerations
+                acc[i,:] += acc_ij
+                acc[j,:] -= mass_1 * acc_ij / mass_2 # because acc_2nbody already multiply by m[j]
+        
+        
+
+  if softening == 0.:
+      # If no softening compute acceleration values
+      direct_acc_no_softening()
+
+  else: print("non ho ancora implementato la funzione di softening, sorry")
+
+
+  return (acc,jerk,pot)
+
+
+def acceleration_direct_vectorised(particles: Particles, softening: float =0.) \
+        -> Tuple[npt.NDArray[np.float64],Optional[npt.NDArray[np.float64]],Optional[npt.NDArray[np.float64]]]:
+    
+    #acc  = np.zeros(len(particles))
+    jerk = None
+    pot = None
+
+    pos  = particles.pos
+    #vel  = particles.vel
+    mass = particles.mass
+    #N    = len(particles)
+
+    x = pos[:,0]
+    y = pos[:,1]
+    z = pos[:,2]
+
+    # Coordinate-wise distance for every particle.
+    # dx[0,:] will be an array related to particle i = 0 where each position j is the diffrence x_0 - x_j
+    # For example dx[0,3] is the difference of the x coordinate of particle 0 with particle 3 (x_0 - x_3)
+    dx = x.T - x 
+    dy = y.T - y
+    dz = z.T - z 
+    
+    # increments is a data-cube of shape 3xNxN containing all coordinate-distances 
+    # It contains the results of r_i - r_j (see equation at p.13 in "Lecture3 - Forces" slides)
+    increments = np.array(dx,dy,dz)
+    
+    # To compute |r_i - r_j|**3 it is necessary to take the norm "channel wise", 
+    # i.e. computed on the first axis (from left) of increments array. 
+    # increments[:,0,1] = [x_0 - x_1, y_0-y_1, z_0-z_1] 
+    distance = np.linalg.norm(increments,axis=0)
+    
+    # Now acceleration for every particle can be computed.
+    # Sum is run over rows; the result will be a NxNx1 matrix,
+    # where the first axis represents the coordinates (x,y,z) and the second axis is the particle.
+    # acceleration_matrix[:,0,1] = [a_x,a_y,a_z] of particle 0 .
+    acceleration_matrix = - np.sum(mass * increments / distance**3, axis=1)
+
+
+
+
+
