8ff72beb2d9afe0af25aa5024bc1ff61d71ffe5c
diegobonato <diegobonato99@gmail.com>
2023-11-06 15:35:02 +0000

    benchmark done

--- assignment2/benchmark.py
+++ assignment2/benchmark.py
@@ -6,41 +6,64 @@ from fireworks.ic import ic_random_uniform as ic
 import time
 from tqdm import tqdm
 
+
+overwrite= True
+
+def get_time(func,x):
+    t1=time.perf_counter()
+    func(x)
+    t2=time.perf_counter()
+    dt=t2-t1 # time elapsed from t1 to t2 in s
+
+    return dt
+
 # Select a grid of values for the number of particles to test.
 N   = np.arange(1,1e3,100)
-
+#N = [1,100,1e3,5e3,1e4]
 # For each N, generate random initial conditions using the function you added in fireworks in task A.
 position_bound = [-100,200]
 velocity_bound = position_bound*2
 mass_bound  = [0.1,1e3]
 
-timings = []
+timings_direct     = []
+timings_vectorised = []
+timings_pyfalcon   = []
+
 for n in tqdm(N,desc="Iterating over N",unit="N") :
    
     # ic returns already an instance of Particles
     part = ic(N=int(n) , position_bound=position_bound, velocity_bound=velocity_bound, mass_bound=mass_bound)
 
-    print("particle",part)
     # estimate the particles acceleration using the three implemented method in the submodule
     # evaluate the time required to complete the acceleration estimate.
 
-    fdyn.acceleration_direct
-    fdyn.acceleration_direct_vectorised
-    fdyn.acceleration_pyfalcon
-    t1=time.perf_counter()
-    func(part)
-    t2=time.perf_counter()
-    dt=t2-t1 # time elapsed from t1 to t2 in s
+    timings_direct.append(get_time(func=fdyn.acceleration_direct,x=part))
+    timings_vectorised.append(get_time(func=fdyn.acceleration_direct_vectorised,x=part))
+    timings_pyfalcon.append(get_time(func=fdyn.acceleration_pyfalcon,x=part))
 
-    timings.append(dt)
+# Maybe put a check if this file already exists
 
-    print(f"Function {func} took {dt} seconds")
+if overwrite:
 
-    with open("benchmark_results.txt","a") as f:
-        f.write(f"Function {func} took {dt} seconds\n")
+    with open("benchmark_results2.txt","a") as f:
+        f.write(f"{timings_direct}\n f{timings_vectorised}\n f{timings_pyfalcon}")
 
 
-print("ho finito")
+plt.plot(N,timings_direct,marker="o",label="direct")
+plt.plot(N,timings_vectorised,marker="o",label="vectorised")
+plt.plot(N,timings_pyfalcon,marker="o",label="pyfalcon")
+
+plt.grid()
+plt.xlabel("N")
+plt.ylabel("log(Time) [s]")
+plt.yscale("log")
+
+
+plt.legend()
+
+#plt.savefig("benchmark_plot2.png")
+
+plt.show()
 
 
 
