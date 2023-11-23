04b99fbb60354f888e859f4ff5a4d6145b02624d
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2023-11-23 22:13:00 +0100

    edited some stuff. i'm tired.

--- Fireworks/fireworks_test/ass3_all.py
+++ Fireworks/fireworks_test/ass3_all.py
@@ -10,7 +10,7 @@ import fireworks.nbodylib.integrators as fint
 mass1 = 2.0
 mass2 = 1.0
 rp = 2.0
-e = 0.6  # Set eccentricity to 0 for a circular orbit
+e = 0.0  # Set eccentricity to 0 for a circular orbit
 part = fic.ic_two_body(mass1=mass1, mass2=mass2, rp=rp, e=e)
 # print(part.pos, part.vel, part.mass)
 Etot_0, _, _ = part.Etot()
@@ -23,7 +23,11 @@ Tperiod = 2 * np.pi * np.sqrt(a**3 / (mass1 + mass2))
 ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod])
 np.savetxt('./fireworks_test/data/ass_3/ic_param.txt', ic_param)
 
-dfs = []
+pos_i = np.asarray((2, 3))
+vel_i = np.asarray(part.vel)
+acc_i = np.asarray(part.vel)
+mass_i = []
+Etot_i = []
 
 integrator_dict = {'Euler_base': fint.integrator_template,
                 'Euler_modified': fint.integrator_euler,
@@ -31,49 +35,35 @@ integrator_dict = {'Euler_base': fint.integrator_template,
                 'Leapfrog': fint.integrator_leapfrog,
                 'RK4': fint.integrator_rk4}
 
-tstep_list = [1]
+tstep_list = [1, 0.01]
 
 for integrator_name, integrator in integrator_dict.items():
     for tstep in tstep_list:
 
-        pos_i = []
-        vel_i = []
-        acc_i = []
-        mass_i = []
-        Etot_i = []
+        pos_ij = np.asarray(part.pos)
+        vel_ij = np.asarray(part.vel)
+        acc_ij = np.asarray(part.vel)
+        mass_ij = []
+        Etot_ij = []
 
         t = 0.
 
         while t < 10*Tperiod:
             part, _, acc, _, _ = integrator(part, tstep=tstep, acceleration_estimator=fdyn.acceleration_pyfalcon)
-            pos_i.append(part.pos)
-            vel_i.append(part.vel)
-            mass_i.append(part.mass)
-            acc_i.append(acc)
+            pos_ij = np.vstack(part.pos)
+            vel_ij = np.vstack(part.vel)
+            mass_ij.append(part.mass)
+            acc_ij = np.vstack(acc)
 
             Etot_j, _, _ = part.Etot()
-            Etot_i.append(Etot_j)
+            Etot_ij.append(Etot_j)
 
             t += tstep
-
-    # Create a DataFrame for the current (integrator, tstep) pair
-    df = pd.DataFrame({
-        'pos': pos_i,
-        'vel': vel_i,
-        'mass': mass_i,
-        'acc': acc_i,
-        'Etot': Etot_i
-    })
-    df.index = pd.MultiIndex.from_product([[integrator_name], [tstep], df.index], names=['integrator', 'tstep', 'i'])
-
-    # Add the DataFrame to the list
-    dfs.append(df)
-
-# Concatenate the DataFrames into a single DataFrame with a multi-index
-# df = pd.concat(dfs)
-
-# Save the DataFrame to a CSV file
-df.to_csv("./data_dict.csv")
+        pos_i = np.vstack(pos_ij)
+        vel_i = np.vstack(vel_ij)
+        mass_i.append(mass_ij)
+        acc_i = np.vstack(acc_ij)
+        Etot_i.append(Etot_ij)
 
 
 # pos_i = np.array(pos_i)
@@ -82,11 +72,11 @@ df.to_csv("./data_dict.csv")
 # mass_i = np.array(mass_i)
 # Etot_i = np.array(Etot_i)
 
-# np.save('./fireworks_test/data/ass_3/pos_i.npy', pos_i)
-# np.save('./fireworks_test/data/ass_3/vel_i.npy', vel_i)
-# np.save('./fireworks_test/data/ass_3/acc_i.npy', acc_i)
-# np.save('./fireworks_test/data/ass_3/mass_i.npy', mass_i)
-# np.save('./fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
+np.save('./fireworks_test/data/ass_3/pos_i.npy', pos_i)
+np.save('./fireworks_test/data/ass_3/vel_i.npy', vel_i)
+np.save('./fireworks_test/data/ass_3/acc_i.npy', acc_i)
+np.save('./fireworks_test/data/ass_3/mass_i.npy', mass_i)
+np.save('./fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
 
 # print('pos_i', pos_i.shape)
 # print('vel_i', vel_i.shape)
