18bff02d2301e3cd52cf6cf93593cd44eb47df4f
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2023-11-23 13:31:14 +0100

    fixed typos

--- Fireworks/fireworks/particles.py
+++ Fireworks/fireworks/particles.py
@@ -76,10 +76,10 @@ class Particles:
 
         self.vel = np.array(np.atleast_2d(velocity), dtype=float)
         if self.vel.shape[1] != 3: print(f"Input velocity should contain a Nx3 array, current shape is {self.pos.shape}")
-        if len(self.vel) != len(self.pos): print(f"Position and velocity in input have not the same number of elemnts")
+        if len(self.vel) != len(self.pos): print(f"Position and velocity in input have not the same number of elements")
 
         self.mass = np.array(np.atleast_1d(mass), dtype=float)
-        if len(self.mass) != len(self.pos): print(f"Position and mass in input have not the same number of elemnts")
+        if len(self.mass) != len(self.pos): print(f"Position and mass in input have not the same number of elements")
 
         self.ID=np.arange(len(self.mass), dtype=int)
 
