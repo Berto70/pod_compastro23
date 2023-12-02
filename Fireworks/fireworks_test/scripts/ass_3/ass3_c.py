69e4ea8e788f510aca289f5526d2a59ea7c8d7ff
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2023-12-02 11:56:17 +0100

    minor changes. testing

--- Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
+++ Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
@@ -1,87 +1,16 @@
-<<<<<<< HEAD
 import numpy as np
 import fireworks.ic as fic
 import matplotlib.pyplot as plt
 from fireworks.particles import Particles
 import fireworks.nbodylib.dynamics as fdyn
 import fireworks.nbodylib.integrators as fint
-
-# Initialize two stars in a circular orbit
-mass1 = 4.0
-mass2 = 2.0
-rp = 2
-e = 0.0  # Set eccentricity to 0 for a circular orbit
-part = fic.ic_two_body(mass1=mass1, mass2=mass2, rp=rp, e=e)
-# print(part.pos, part.vel, part.mass)
-Etot_0, _, _ = part.Etot()
-
-# Calculate the binary period Tperiod
-a = rp / (1 - e)  # Semi-major axis
-Tperiod = 2 * np.pi * np.sqrt(a**3 / (mass1 + mass2))
-# print("Binary Period Tperiod:", Tperiod)
-
-t = 0.
-tstep = 0.01
-N_end = 10
-
-ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, tstep, N_end])
-np.savetxt('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/ic_param.txt', ic_param)
-
-pos_i = []
-vel_i = []
-acc_i = []
-mass_i = []
-Etot_i = []
-
-
-while t < N_end*Tperiod:
-    part, _, acc, _, _ = fint.integrator_tsunami(part, tstep=tstep, acceleration_estimator=fdyn.acceleration_direct_vectorized)
-    pos_i.append(part.pos)
-    vel_i.append(part.vel)
-    mass_i.append(part.mass)
-    acc_i.append(acc)
-
-    Etot_j, _, _ = part.Etot()
-    Etot_i.append(Etot_j)
-
-    t += tstep
-
-pos_i = np.array(pos_i)
-vel_i = np.array(vel_i)
-acc_i = np.array(acc_i)
-mass_i = np.array(mass_i)
-Etot_i = np.array(Etot_i)
-
-np.save('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/pos_i.npy', pos_i)
-np.save('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/vel_i.npy', vel_i)
-np.save('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/acc_i.npy', acc_i)
-np.save('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/mass_i.npy', mass_i)
-np.save('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
-
-print('pos_i', pos_i.shape)
-print('vel_i', vel_i.shape)
-
-# peri = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).min(axis=1)
-# apo = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).max(axis=1)
-
-# np.save('./fireworks_test/data/ass_3/periastron.npy', peri)
-# np.save('./fireworks_test/data/ass_3/apoastron.npy', apo)
-
-# print('peri', peri.shape)
-# print('apo', apo.shape)
-=======
-import numpy as np
-import fireworks.ic as fic
-import matplotlib.pyplot as plt
-from fireworks.particles import Particles
-import fireworks.nbodylib.dynamics as fdyn
-import fireworks.nbodylib.integrators as fint
+import fireworks.nbodylib.timesteps as fts
 
 # Initialize two stars in a circular orbit
 mass1 = 15.0
-mass2 = 2.0
-rp = 2
-e = 0.0  # Set eccentricity to 0 for a circular orbit
+mass2 = 1.0
+rp = 0.01
+e = 0.9  # Set eccentricity to 0 for a circular orbit
 part = fic.ic_two_body(mass1=mass1, mass2=mass2, rp=rp, e=e)
 # print(part.pos, part.vel, part.mass)
 Etot_0, _, _ = part.Etot()
@@ -93,10 +22,7 @@ Tperiod = 2 * np.pi * np.sqrt(a**3 / (mass1 + mass2))
 
 t = 0.
 tstep = 0.0001
-N_end = 10
-
-ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, tstep, N_end])
-np.savetxt('./fireworks_test/data/ass_3/ic_param.txt', ic_param)
+N_end = 50
 
 pos_i = []
 vel_i = []
@@ -106,7 +32,8 @@ Etot_i = []
 
 
 while t < N_end*Tperiod:
-    part, _, acc, _, _ = fint.integrator_tsunami(part, tstep=tstep, acceleration_estimator=fdyn.acceleration_direct_vectorized)
+    # tstep = 0.0001
+    part, _, acc, _, _ = fint.integrator_leapfrog(part, tstep=tstep, acceleration_estimator=fdyn.acceleration_direct_vectorized)
     pos_i.append(part.pos)
     vel_i.append(part.vel)
     mass_i.append(part.mass)
@@ -116,6 +43,7 @@ while t < N_end*Tperiod:
     Etot_i.append(Etot_j)
 
     t += tstep
+    # tstep = fts.euler_timestep(part, eta=0.0001, acc = acc)
 
 pos_i = np.array(pos_i)
 vel_i = np.array(vel_i)
@@ -129,15 +57,17 @@ np.save('./fireworks_test/data/ass_3/acc_i.npy', acc_i)
 np.save('./fireworks_test/data/ass_3/mass_i.npy', mass_i)
 np.save('./fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
 
+ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, tstep, N_end])
+np.savetxt('./fireworks_test/data/ass_3/ic_param.txt', ic_param)
+
 print('pos_i', pos_i.shape)
 print('vel_i', vel_i.shape)
 
-peri = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).min(axis=1)
-apo = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).max(axis=1)
+# peri = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).min(axis=1)
+# apo = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).max(axis=1)
 
-np.save('./fireworks_test/data/ass_3/periastron.npy', peri)
-np.save('./fireworks_test/data/ass_3/apoastron.npy', apo)
+# np.save('./fireworks_test/data/ass_3/periastron.npy', peri)
+# np.save('./fireworks_test/data/ass_3/apoastron.npy', apo)
 
-print('peri', peri.shape)
-print('apo', apo.shape)
->>>>>>> fe414b24627074b5aaf73fc276612dc7337229b4
+# print('peri', peri.shape)
+# print('apo', apo.shape)
--- Fireworks/fireworks_test/scripts/ass_3/plots_ass3.py
+++ Fireworks/fireworks_test/scripts/ass_3/plots_ass3.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
-ic_param = np.genfromtxt('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/ic_param.txt')
+ic_param = np.genfromtxt('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/ic_param.txt')
 mass1 = ic_param[0]
 mass_2 = ic_param[1]
 rp = ic_param[2]
@@ -10,14 +10,14 @@ e = ic_param[3]
 a = ic_param[4]
 Etot_0 = ic_param[5]
 Tperiod = ic_param[6]
-tstep = ic_param[7]
-N_end = ic_param[8]
+# tstep = ic_param[7]
+N_end = ic_param[7]
 
-pos_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/pos_i.npy', allow_pickle=True)
-vel_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/vel_i.npy', allow_pickle=True)
-acc_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/acc_i.npy', allow_pickle=True)
-mass_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/mass_i.npy', allow_pickle=True)
-Etot_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/Etot_i.npy', allow_pickle=True)
+pos_i = np.load('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/pos_i.npy', allow_pickle=True)
+vel_i = np.load('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/vel_i.npy', allow_pickle=True)
+acc_i = np.load('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/acc_i.npy', allow_pickle=True)
+mass_i = np.load('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/mass_i.npy', allow_pickle=True)
+Etot_i = np.load('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/data/ass_3/Etot_i.npy', allow_pickle=True)
 
 # print(Etot_i)
 # print(Etot_i.shape)
@@ -27,7 +27,7 @@ Etot_i = np.load('ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3
 
 fig, ax = plt.subplots(2, 1, figsize=(10, 20))
 # Plot position on x-y plane
-ax[0].plot(pos_i[:, :, 0], pos_i[:, :, 1], label=['Body 1', 'Body 2'])
+ax[0].plot(pos_i[:, :, 0], pos_i[:, :, 1], label=['Body 1', 'Body 2'], linewidth=0.0001)
 ax[0].set_xlabel('X')
 ax[0].set_ylabel('Y')
 ax[0].legend()
@@ -39,7 +39,7 @@ ax[0].set_title('Position on X-Y Plane')
 # ax[1].set_ylabel('v_y')
 # ax[1].legend()
 # ax[1].set_title('Velocity on Y Plane')
-plt.savefig('ext_volume/pod_compastro23/Fireworks/fireworks_test/plots/ass_3/pos_vel_plot.pdf')
+plt.savefig('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/plots/ass_3/pos_vel_plot.pdf')
 
 # fig1, ax1 = plt.subplots(1, 1, figsize=(8,5))
 # # Plot position on x-y plane
@@ -58,7 +58,7 @@ ax2.set_xlabel('absolute time')
 ax2.set_ylabel('|(E-E0)/E0|')
 ax2.set_yscale('log')  # Set y-axis to be in log scale
 ax2.set_title('ΔE evolution')
-plt.savefig('ext_volume/pod_compastro23/Fireworks/fireworks_test/plots/ass_3/Etot.pdf')
+plt.savefig('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/plots/ass_3/Etot.pdf')
 
 # # Plot the precession of perihelion and aphelion
 # fig2, ax2 = plt.subplots(1, 1, figsize=(8, 5))
