47e8f903e232281d1c56f6b66372c51f8845bb31
diegobonato <diegobonato99@gmail.com>
2023-12-04 20:07:30 +0000

    add evolver func

---
+++ assignment3/evolver.py
@@ -0,0 +1,49 @@
+import fireworks
+from fireworks.nbodylib import dynamics as dyn
+from fireworks.nbodylib import integrators as intg
+from fireworks import ic
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def evolve(particles,integrator,tstep = .001):
+        
+
+    period = 10
+    tot_time = 0 
+
+    acc_list = []
+    potential_list = []
+    positions = []
+    velocities = []
+    energy = []
+    jerk_ = []
+
+    init_energy = particles.Etot()
+
+    while tot_time < 10*period:
+        tot_time +=tstep
+
+        # Evolve the binary 
+
+        (particles, tstep, acc, jerk, potential) = integrator(particles=particles, 
+                                                                            tstep=tstep,
+                                                                            acceleration_estimator=dyn.acceleration_direct_vectorized,
+                                                                            softening=0.)
+        
+        acc_list.append(acc)
+        potential_list.append(potential)
+        positions.append(particles.pos)
+        velocities.append(particles.vel)
+        energy.append(particles.Etot())
+        jerk_.append(jerk)
+    
+    
+    
+    potential_list = np.array(potential_list)
+    positions = np.array(positions).reshape(len(positions),2,3)
+    velocities = np.array(velocities).reshape(len(velocities),2,3)
+    energy = np.array(energy)
+    return (acc_list, potential_list, positions, velocities, energy,init_energy,jerk_)
+
+
