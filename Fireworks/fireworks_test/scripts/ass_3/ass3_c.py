bd6122ae90e52940abea43c2e114ab445a656a22
Gabriele Bertinelli <61018656+Berto70@users.noreply.github.com>
2023-12-11 11:47:23 +0100

    failed tsunami integrator test

--- Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
+++ Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
@@ -22,7 +22,7 @@ Tperiod = 2 * np.pi * np.sqrt(a**3 / (mass1 + mass2))
 # print("Binary Period Tperiod:", Tperiod)
 
 t = 0.
-tstep = 0.000001
+# tstep = 0.0001
 N_end = 65
 
 pos_i = []
@@ -30,6 +30,7 @@ vel_i = []
 acc_i = []
 mass_i = []
 Etot_i = []
+tstep_i = []
 
 position = np.array([[0,0,0],
                      [0.5,0.866,9],
@@ -45,45 +46,68 @@ mass = np.array([3,4,5])
 particles = Particles(position, vel, mass)
 
 
-pbar = tqdm(total=N_end*Tperiod/tstep)
+# pbar = tqdm(total=N_end*Tperiod/tstep)
 
-while t < N_end*Tperiod:
-    # t += tstep
-    part, _, acc, jerk, _ = fint.integrator_leapfrog(particles, 
-                                                    tstep=tstep, 
-                                                    acceleration_estimator=fdyn.acceleration_direct_vectorized
-                                                )
+tstart=0
+
+tintermediate=[5,10,15,20,30,40,50,60,65]
+
+tcurrent=0
+
+for t in tintermediate:
+    tstep = t-tcurrent
+
+    part, efftime, _, _, _ = fint.integrator_tsunami(particles, tstep)
+     # Here we can save stuff, plot stuff, etc.
     pos_i.append(part.pos)
     vel_i.append(part.vel)
     mass_i.append(part.mass)
-    acc_i.append(acc)
-
     Etot_j, _, _ = part.Etot()
     Etot_i.append(Etot_j)
 
-    # Update the progress bar
-    pbar.update(1)
+    tcurrent += efftime
 
-    t += tstep
-    tstep = fts.euler_timestep(part, eta=0.0001, acc = acc)
+# while t < N_end*Tperiod:
+#     # t += tstep
+#     part, tstep, acc, _, _ = fint.integrator_rk4(particles, 
+#                                                     tstep=tstep, 
+#                                                     acceleration_estimator=fdyn.acceleration_pyfalcon
+#                                                 )
+#     pos_i.append(part.pos)
+#     vel_i.append(part.vel)
+#     mass_i.append(part.mass)
+#     acc_i.append(acc)
+#     tstep_i.append(tstep)
+
+#     Etot_j, _, _ = part.Etot()
+#     Etot_i.append(Etot_j)
+
+#     # Update the progress bar
+#     pbar.update(1)
+
+#     t += tstep
+#     # tstep = fts.euler_timestep(part, eta=0.0001, acc = acc)
 
 pos_i = np.array(pos_i)
 vel_i = np.array(vel_i)
-acc_i = np.array(acc_i)
+# acc_i = np.array(acc_i)
 mass_i = np.array(mass_i)
 Etot_i = np.array(Etot_i)
+tstep_i = np.array(tintermediate)
 
-np.save('./fireworks_test/data/ass_3/pos_i.npy', pos_i)
-np.save('./fireworks_test/data/ass_3/vel_i.npy', vel_i)
-np.save('./fireworks_test/data/ass_3/acc_i.npy', acc_i)
-np.save('./fireworks_test/data/ass_3/mass_i.npy', mass_i)
-np.save('./fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
+np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/pos_i.npy', pos_i)
+np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/vel_i.npy', vel_i)
+# np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/acc_i.npy', acc_i)
+np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/mass_i.npy', mass_i)
+np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/Etot_i.npy', Etot_i)
+np.save('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/tstep_i.npy', tstep_i)
 
 ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, tstep, N_end])
-np.savetxt('./fireworks_test/data/ass_3/ic_param.txt', ic_param)
+np.savetxt('/ca23/ext_volume/pod_compastro23/Fireworks/fireworks_test/data/ass_3/ic_param.txt', ic_param)
 
 print('pos_i', pos_i.shape)
 print('vel_i', vel_i.shape)
+print('tsteo_i', tstep_i.shape)
 
 # # peri = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).min(axis=1)
 # # apo = np.sqrt(pos_i[:, :, 0]**2 + pos_i[:, :, 1]**2).max(axis=1)
