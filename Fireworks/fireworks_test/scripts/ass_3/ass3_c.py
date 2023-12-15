120c3dfe035ea347b07652c2d0097832d992ffe6
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2023-12-15 16:44:34 +0100

    fixed code organization

--- Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
+++ Fireworks/fireworks_test/scripts/ass_3/ass3_c.py
@@ -11,6 +11,7 @@ np.random.seed(9725)
 
 path = "/home/bertinelli/pod_compastro23/Fireworks/fireworks_test"
 
+## TWO-BODY PROBLEM ##
 # Initialize two stars in a circular orbit
 mass1 = 8
 mass2 = 2
@@ -23,103 +24,125 @@ Etot_0, _, _ = part.Etot()
 # Calculate the binary period Tperiod
 a = rp / (1 - e)  # Semi-major axis
 Tperiod = 2 * np.pi * np.sqrt(a**3 / (mass1 + mass2))
-# print("Binary Period Tperiod:", Tperiod)
 
-t = 0.
-tstep = 0.1
-N_end = 10
+## THREE-BODY PROBLEM ##
 
-pos_i = []
-vel_i = []
-acc_i = []
-mass_i = []
-Etot_i = []
-# tstep_i = []
+position = np.array([[0,0,0],
+                         [0.5,0.866,9],
+                         [1,0,0]])
 
-# position = np.array([[0,0,0],
-#                      [0.5,0.866,9],
-#                      [1,0,0]])
+vel = np.array([[0,0,0],
+                [0,0,0],
+                [0,0,0]])
 
-# vel = np.array([[0,0,0],
-#                 [0,0,0],
-#                 [0,0,0]])
+mass = np.array([3,4,5])
 
-# mass = np.array([3,4,5])
+# Create instances of the particles
+particles = Particles(position, vel, mass)
+Etot_0, _, _ = particles.Etot()
 
-# # Create instances of the particles
-# part = Particles(position, vel, mass)
-# Etot_0, _, _ = part.Etot()
 
-## OTHER INTEGRATORS ##
+## TSUNAMI TRUE/FALSE CONDITION ##
+tsunami_true = False
 
-total = N_end*Tperiod/tstep
-pbar = tqdm(total=total)
+if tsunami_true == True: ## TSUNAMI INTEGRATOR ##
+    pos_i = []
+    vel_i = []
+    acc_i = []
+    mass_i = []
+    Etot_i = []
+    tstep_i = []
 
-while t < N_end*Tperiod:
-    # t += tstep
-    part, _, acc, jerk, _ = fint.integrator_rk4(part, tstep, 
-                                                         acceleration_estimator=fdyn.acceleration_direct_vectorized, args={'return_jerk': False})
-    pos_i.append(part.pos)
-    vel_i.append(part.vel)
-    mass_i.append(part.mass)
-    acc_i.append(acc)
+    tstart=0
+    tintermediate=np.linspace(0+0.00001, 65, 100000)
+    tcurrent=0
 
-    Etot_j, _, _ = part.Etot()
-    Etot_i.append(Etot_j)
+    pbar = tqdm(total=len(tintermediate))
 
-    # tstep = fts.adaptive_timestep_jerk(particles=part, eta=10e-4, acc=acc, tmin=0.0001, tmax=0.01)
+    for t in tintermediate:
 
-    tstep = fts.adaptive_timestep(part, integrator=fint.integrator_rk4, int_rank=4, int_args={'particles': part, 'tstep': tstep, 'acceleration_estimator': fdyn.acceleration_direct_vectorized},
-                                   predictor=fint.integrator_heun, pred_rank=2, pred_args={'particles': part, 'tstep': tstep, 'acceleration_estimator': fdyn.acceleration_direct_vectorized},
-                                   epsilon=10e-9, tmax=0.01, tmin=0.0001)
-    
-    t += tstep
-    pbar.update(1)
+        tstep = t-tcurrent
+        if tstep<=0: continue
 
-# ## TSUNAMI INTEGRATOR ##
+        particles, efftime, _, _, _ = fint.integrator_tsunami(particles, tstep)
 
-# tstart=0
-# tintermediate=np.linspace(0+0.00001, 65, 100000)
-# tcurrent=0
+        # Here we can save stuff, plot stuff, etc.
+        pos_i.append(particles.pos.copy())
+        vel_i.append(particles.vel.copy())
+        mass_i.append(particles.mass.copy())
+        Etot_j, _, _ = particles.Etot()
+        Etot_i.append(Etot_j.copy())
+        tstep_i.append(tstep)
 
-# pbar = tqdm(total=len(tintermediate))
+        pbar.update(1)
 
-# for t in tintermediate:
+        tcurrent += efftime
 
-#     tstep = t-tcurrent
-#     if tstep<=0: continue
+else:     ## OTHER INTEGRATORS ##
 
-#     particles, efftime, _, _, _ = fint.integrator_tsunami(particles, tstep)
+    t = 0.
+    tstep = 1
+    N_end = 10
 
-#      # Here we can save stuff, plot stuff, etc.
-#     pos_i.append(particles.pos.copy())
-#     vel_i.append(particles.vel.copy())
-#     mass_i.append(particles.mass.copy())
-#     Etot_j, _, _ = particles.Etot()
-#     Etot_i.append(Etot_j.copy())
-#     tstep_i.append(tstep)
+    pos_i = []
+    vel_i = []
+    acc_i = []
+    mass_i = []
+    Etot_i = []
 
-#     pbar.update(1)
+    total = N_end*Tperiod/tstep
+    pbar = tqdm(total=total)
 
-#     tcurrent += efftime
+    while t < N_end*Tperiod:
+        try:
 
+        # t += tstep
+            part, _, acc, jerk, _ = fint.integrator_rk4(part, tstep, 
+                                                                acceleration_estimator=fdyn.acceleration_direct_vectorized, args={'return_jerk': True})
+            pos_i.append(part.pos)
+            vel_i.append(part.vel)
+            mass_i.append(part.mass)
+            acc_i.append(acc)
+
+            Etot_j, _, _ = part.Etot()
+            Etot_i.append(Etot_j)
+
+            tstep = fts.adaptive_timestep_jerk(jerk=jerk, eta=10e-4, acc=acc, tmin=0.0001, tmax=0.01)
+
+            # tstep = fts.adaptive_timestep(integrator=fint.integrator_rk4, int_rank=4, int_args={'particles': part, 'tstep': tstep, 'acceleration_estimator': fdyn.acceleration_direct_vectorized},
+            #                             predictor=fint.integrator_heun, pred_rank=2, pred_args={'particles': part, 'tstep': tstep, 'acceleration_estimator': fdyn.acceleration_direct_vectorized},
+            #                             epsilon=10e-9, tmax=0.01, tmin=0.0001)
+        
+            t += tstep
+            pbar.update(1)
+        
+        except Exception as exception:
+            print(f"An error occurred: {exception}")
+            break
+
+
+## SAVE DATA ##
+
+ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, N_end])
 
 pos_i = np.array(pos_i)
 vel_i = np.array(vel_i)
 acc_i = np.array(acc_i)
 mass_i = np.array(mass_i)
 Etot_i = np.array(Etot_i)
-# tstep_i = np.array(tstep_i)
+if tsunami_true == True:
+    tstep_i = np.array(tstep_i)
+
+
+np.savetxt(path + '/data/ass_3/ic_param.txt', ic_param)
 
 np.save(path + '/data/ass_3/pos_i.npy', pos_i)
 np.save(path + '/data/ass_3/vel_i.npy', vel_i)
 np.save(path + '/data/ass_3/acc_i.npy', acc_i)
 np.save(path + '/data/ass_3/mass_i.npy', mass_i)
 np.save(path + '/data/ass_3/Etot_i.npy', Etot_i)
-# np.save(path + '/data/ass_3/tstep_i.npy', tstep_i)
-
-ic_param = np.array([mass1, mass2, rp, e, a, Etot_0, Tperiod, N_end])
-np.savetxt(path + '/data/ass_3/ic_param.txt', ic_param)
+if tsunami_true == True:
+    np.save(path + '/data/ass_3/tstep_i.npy', tstep_i)
 
 # print('pos_i', pos_i.shape)
 # print('\nvel_i', vel_i.shape)
