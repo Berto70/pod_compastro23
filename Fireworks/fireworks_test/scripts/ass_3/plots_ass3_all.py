e3a1c31e8cc833be964a9e325160936ac77bfc98
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2023-12-19 16:57:37 +0100

    adaptive tstep distr hist + init (dist vs tstep + energy) scatter

--- Fireworks/fireworks_test/scripts/ass_3/plots_ass3_all.py
+++ Fireworks/fireworks_test/scripts/ass_3/plots_ass3_all.py
@@ -777,6 +777,7 @@ with PdfPages('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/plots/a
 
 ##############################################################################################################
     # TIMESTEP DISTRIBUTION
+    # Page1
 
     custom_cycler9 = (cycler(color=['orange', 'seagreen', 'navy']))
     plt.rc('axes', prop_cycle=custom_cycler9)
@@ -784,21 +785,28 @@ with PdfPages('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/plots/a
     fig9, ax9 = plt.subplots(3, 3, figsize=(40, 40))
     gs9 = GridSpec(3,3)
     # Plot position on x-y plane
-    
-    ax9[2,0].hist(data_00001_tsu[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
-    ax9[2,1].hist(data_0001_tsu[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
-    ax9[2,2].hist(data_001_tsu[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
-    
+
+    ax9[0,0].hist(data_00001_base[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[0,1].hist(data_0001_base[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[0,2].hist(data_001_base[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[0,1].set_title('Euler_base')
+
+    ax9[1,0].hist(data_00001_mod[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[1,1].hist(data_0001_mod[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[1,2].hist(data_001_mod[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[1,1].set_title('Euler_modified')
+
+    ax9[2,0].hist(data_00001_her[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[2,1].hist(data_0001_her[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[2,2].hist(data_001_her[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[2,1].set_title('Hermite')
+
     for i in range(3):
         for j in range(3):
             ax9[i,j].set_xlabel('Time Step')
             ax9[i,j].set_prop_cycle(custom_cycler9)
             # ax9[i,j].yaxis.set_major_formatter(mticker.PercentFormatter(decimals=0, symbol=None, xmax=1.))
-            ax9[i,j].legend()            
-
-
-    # fig9.delaxes(ax9[2,1])
-    # fig9.delaxes(ax9[2,2])
+            ax9[i,j].legend()
 
 
     fig9.suptitle('Adaptive Timesteps Distribution\n(M1=%.1f, M2=%.1f, e=%.1f, rp=%.2f, T=%.2f)'%(mass_1, mass_2, e, rp, Tperiod), 
@@ -806,3 +814,140 @@ with PdfPages('/home/bertinelli/pod_compastro23/Fireworks/fireworks_test/plots/a
 
     pdf.savefig(dpi = 100)
     plt.close()
+
+    # Page2
+
+    custom_cycler9 = (cycler(color=['orange', 'seagreen', 'navy']))
+    plt.rc('axes', prop_cycle=custom_cycler9)
+
+    fig9, ax9 = plt.subplots(3, 3, figsize=(40, 40))
+    gs9 = GridSpec(3,3)
+    # Plot position on x-y plane
+
+    ax9[0,0].hist(data_00001_rk2[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[0,1].hist(data_0001_rk2[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[0,2].hist(data_001_rk2[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[0,1].set_title('RK2-Heun')
+
+    ax9[1,0].hist(data_00001_leap[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[1,1].hist(data_0001_leap[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[1,2].hist(data_001_leap[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[1,1].set_title('Leapfrog')
+
+    ax9[2,0].hist(data_00001_rk4[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[2,1].hist(data_0001_rk4[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[2,2].hist(data_001_rk4[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[2,1].set_title('RK4')
+
+    for i in range(3):
+        for j in range(3):
+            ax9[i,j].set_xlabel('Time Step')
+            ax9[i,j].set_prop_cycle(custom_cycler9)
+            # ax9[i,j].yaxis.set_major_formatter(mticker.PercentFormatter(decimals=0, symbol=None, xmax=1.))
+            ax9[i,j].legend()
+
+    #Page3
+
+    custom_cycler9 = (cycler(color=['orange', 'seagreen', 'navy']))
+    plt.rc('axes', prop_cycle=custom_cycler9)
+
+    fig9, ax9 = plt.subplots(1, 3, figsize=(40, 17))
+    gs9 = GridSpec(1,3)
+    # Plot position on x-y plane
+    ax9[0].set_position([0.1, 0.1, 0.25, 0.7])  # Adjust these values as needed
+    ax9[1].set_position([0.4, 0.1, 0.25, 0.7])
+    ax9[2].set_position([0.7, 0.1, 0.25, 0.7])
+    
+    ax9[0].hist(data_00001_tsu[:, 5], density=True, alpha=0.8, label='h=0.00001', histtype='step', linewidth=5, cumulative=True)
+    ax9[1].hist(data_0001_tsu[:, 5], density=True, alpha=0.8, label='h=0.0001', histtype='step', linewidth=5)
+    ax9[2].hist(data_001_tsu[:, 5], density=True, alpha=0.8, label='h=0.001', histtype='step', linewidth=5)
+    ax9[1].set_title('Tsunami')
+    
+    for j in range(3):
+        ax9[j].set_xlabel('Time Step')
+        ax9[j].set_prop_cycle(custom_cycler9)
+        # ax9[j].yaxis.set_major_formatter(mticker.PercentFormatter(decimals=0, symbol=None, xmax=1.))
+        ax9[j].legend()            
+
+
+    # fig9.suptitle('Adaptive Timesteps Distribution\n(M1=%.1f, M2=%.1f, e=%.1f, rp=%.2f, T=%.2f)'%(mass_1, mass_2, e, rp, Tperiod), 
+    #              fontsize=52, fontweight='600')
+
+    pdf.savefig(dpi = 100)
+    plt.close()
+
+##############################################################################################################
+    # TIMESTEP BOXPLOT
+
+    ints = np.array(['Euler_b', 'Euler_mod', 'Her', 'RK2', 'Leap', 'RK4', 'Tsu'])
+
+    custom_cycler10 = (cycler(color=['tab:blue', 'tab:orange']))
+    plt.rc('axes', prop_cycle=custom_cycler10)
+
+    fig10, ax10 = plt.subplots(1, 3, figsize=(40, 17))
+
+    ax10[0].set_position([0.1, 0.1, 0.25, 0.7])  # Adjust these values as needed
+    ax10[1].set_position([0.4, 0.1, 0.25, 0.7])
+    ax10[2].set_position([0.7, 0.1, 0.25, 0.7])
+
+    ax10[0].boxplot([data_00001_base[:, 5], data_00001_mod[:, 5], data_00001_her[:, 5], 
+                     data_00001_rk2[:, 5], data_00001_leap[:, 5], data_00001_rk4[:, 5], data_00001_tsu[:, 5]],
+                     labels=ints, notch=True, vert=True, patch_artist=True, boxprops=dict(alpha=0.8), showfliers=False)
+    ax10[0].set_title('h=0.00001')
+
+    ax10[1].boxplot([data_0001_base[:, 5], data_0001_mod[:, 5], data_0001_her[:, 5],
+                        data_0001_rk2[:, 5], data_0001_leap[:, 5], data_0001_rk4[:, 5], data_0001_tsu[:, 5]],
+                        labels=ints, notch=True, vert=True, patch_artist=True, boxprops=dict(alpha=0.8), showfliers=False)
+    ax10[1].set_title('h=0.0001')
+
+    ax10[2].boxplot([data_001_base[:, 5], data_001_mod[:, 5], data_001_her[:, 5],
+                    data_001_rk2[:, 5], data_001_leap[:, 5], data_001_rk4[:, 5], data_001_tsu[:, 5]],
+                    labels=ints, notch=True, vert=True, patch_artist=True, boxprops=dict(alpha=0.8), showfliers=False)
+    ax10[2].set_title('h=0.001')
+
+    fig10.suptitle('Adaptive Timesteps\n(M1=%.1f, M2=%.1f, e=%.1f, rp=%.2f, T=%.2f)'%(mass_1, mass_2, e, rp, Tperiod),
+                     fontsize=52, fontweight='600')
+    
+    for i in range(3):
+        ax10[i].set_xlabel('Integrator')
+        ax10[i].set_ylabel('Time Step')
+        # ax10[i].set_yscale('log')
+        ax10[i].yaxis.grid(True, which='major', alpha=0.5)
+        ax10[i].set_prop_cycle(custom_cycler10)
+
+        # ax10[i].yaxis.set_minor_locator(locminy)
+        # ax10[i].yaxis.set_major_locator(locmajy)
+        # ax10[i].yaxis.set_minor_formatter(mticker.NullFormatter())
+        # ax10[i].yaxis.set_major_formatter(mticker.LogFormatterSciNotation())
+
+    pdf.savefig(dpi=100)
+    plt.close()
+
+##############################################################################################################
+    
+    def distance(x1, y1, x2, y2):
+        return np.sqrt((x2-x1)**2 + (y2-y1)**2)
+    
+    fig11, ax11 = plt.subplots(1, 3, figsize=(40, 17))
+
+    ax11[0].set_position([0.1, 0.1, 0.25, 0.7])  # Adjust these values as needed
+    ax11[1].set_position([0.4, 0.1, 0.25, 0.7])
+    ax11[2].set_position([0.7, 0.1, 0.25, 0.7])
+    
+    ax11[0].scatter(distance(data_00001_base[:,0], data_00001_base[:,1], data_00001_base[:,2], data_00001_base[:,3]), data_00001_base[:,5],
+                    c=data_00001_base[:,4])
+    ax11[0].set_title('h=0.00001')
+
+    ax11[1].scatter(distance(data_0001_base[:,0], data_0001_base[:,1], data_0001_base[:,2], data_0001_base[:,3]), data_0001_base[:,5],
+                    c=data_0001_base[:,4])
+    ax11[1].set_title('h=0.0001')
+
+    ax11[2].scatter(distance(data_001_base[:,0], data_001_base[:,1], data_001_base[:,2], data_001_base[:,3]), data_001_base[:,5],
+                    c=data_001_base[:,4])
+    ax11[2].set_title('h=0.001')
+
+    fig11.suptitle('dist vs tstep\n(M1=%.1f, M2=%.1f, e=%.1f, rp=%.2f, T=%.2f)'%(mass_1, mass_2, e, rp, Tperiod),
+                    fontsize=52, fontweight='600')                    
+    
+    pdf.savefig(dpi=100)
+    plt.close()             
