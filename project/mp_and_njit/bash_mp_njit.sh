f28cf96441126a75da8ba776f879295735195b03
diegobonato <diegobonato99@gmail.com>
2024-02-19 20:44:24 +0100

    mp and njit mod I dont know what

--- project/mp_and_njit/bash_mp_njit.sh
+++ project/mp_and_njit/bash_mp_njit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for n_sim in {1..5};do
-    for n_particles in {250..2000..250}; do
-        python mp_and_njit.py $n_particles $n_sim
+for i in {1..100};do
+    for n_sim in {1..5};do
+        for n_particles in {250..2000..250}; do
+            python mp_and_njit.py $n_particles $n_sim
+        done
     done
 done
 
