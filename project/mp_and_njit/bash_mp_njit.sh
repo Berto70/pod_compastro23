9d6c9d555a3b2c1b2a12660fdefda62ccb2ce3cc
diegobonato <diegobonato99@gmail.com>
2024-02-19 12:28:31 +0000

    mod bash

--- project/mp_and_njit/bash_mp_njit.sh
+++ project/mp_and_njit/bash_mp_njit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-for n_particles in {250..2000..250}; do
-    python numba_experiments.py $n_particles
+for n_sim in {1..5};do
+    for n_particles in {250..2000..250}; do
+        python numba_experiments.py $n_particles $n_sim
+    done
 done
 
